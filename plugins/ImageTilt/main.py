b402e09b0735a9a5b8ed08f7ad9cd6a7a8e2dfb2
Tumppi066 <83072683+Tumppi066@users.noreply.github.com>
2024-02-19 23:30:30 +0200

    Take image based in distance.

--- plugins/ImageTilt/main.py
+++ plugins/ImageTilt/main.py
@@ -33,10 +33,15 @@ import time
 from scipy import ndimage
 import math
 
-pixelMultiplier = -1.8
+pixelMultiplier = -2  # multiplier for the pixel to meter conversion
 atResolution = 301  # scaler for the multiplier 
 angle = 25
-secondsToCapture = 10
+
+metersPerImage = 2 # The amount of meters between images
+# or
+rotationPerImage = 5 # The amount of degrees between images
+# and
+maxDistance = 200 # to delete the image
 def vertical_perspective_warp(image):
     global angle
     height, width = image.shape[:2]
@@ -91,6 +96,10 @@ pastImages = [] # Will save the last 5s of images
 def plugin(data):
     try:
         frame = data["frame"]
+        width = frame.shape[1]
+        height = frame.shape[0]
+        cv2.rectangle(frame, (0,0), (round(width/6),round(height/3)),(0,0,0),-1)
+        cv2.rectangle(frame, (width,0), (round(width-width/6),round(height/3)),(0,0,0),-1)
         centerCoord = getTiltedCenterCoordinate(frame.shape[1], frame.shape[0])
         pxPerMeter = pixelMultiplier * (frame.shape[0] / atResolution)
         
@@ -100,40 +109,66 @@ def plugin(data):
         # Save the image, timestamp and position to the list
         position = (data["api"]["truckPlacement"]["coordinateX"], data["api"]["truckPlacement"]["coordinateZ"])
         rotation = (data["api"]["truckPlacement"]["rotationX"], data["api"]["truckPlacement"]["rotationY"], data["api"]["truckPlacement"]["rotationZ"])
-        rotatedFrame = rotateImage(tiltedFrame, rotation[0]*360, center=centerCoord)
-        pastImages.append((rotatedFrame, timestamp, position, rotation))
-        # Check if the first image is older than the given time
-        while pastImages[0][1] < timestamp - secondsToCapture:
-            pastImages.pop(0)
+        if len(pastImages) > 0:
+            # Check distance to the last image
+            lastPosition = pastImages[-1][2]
+            distance = math.sqrt((position[0] - lastPosition[0])**2 + (position[1] - lastPosition[1])**2)
+            # print(distance)
+            if distance > metersPerImage:
+                rotatedFrame = rotateImage(tiltedFrame, rotation[0]*360, center=centerCoord)
+                pastImages.append((rotatedFrame, timestamp, position, rotation))
+
+            # Check rotation to the last image
+            lastRotation = pastImages[-1][3]
+            rotationDistance = math.sqrt((rotation[0] - lastRotation[0])**2 + (rotation[1] - lastRotation[1])**2 + (rotation[2] - lastRotation[2])**2)
+            if rotationDistance > rotationPerImage:
+                rotatedFrame = rotateImage(tiltedFrame, rotation[0]*360, center=centerCoord)
+                pastImages.append((rotatedFrame, timestamp, position, rotation))
+        else:
+            rotatedFrame = rotateImage(tiltedFrame, rotation[0]*360, center=centerCoord)
+            pastImages.append((rotatedFrame, timestamp, position, rotation))
             
         # Draw the last 5s of images as the frame. The last image will be the most visible, with the first image having an alpha of 0
         frame = np.zeros((frame.shape[0], frame.shape[1], 3), dtype=np.uint8)
         length = len(pastImages)
-        imagePerFrame = 10 # int(length)
         counter = 0
         for i, (image, timestamp, position, rotation) in enumerate(pastImages):
-            if counter < imagePerFrame and not i == length - 1:
-                counter += 1
-                continue
-            
-            counter = 0
             # Set alpha to between 1 and 0 depending on the position in the list
-            alpha = (i / length)
+            alpha = 1
+            # alpha = (i / length)
             # Apply b&w filter
             image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
-            # Move the image up 4/5 of the screen
-            image = np.roll(image, int(frame.shape[0] / 5)*4, axis=0)
+            # Move the image up
+            image = np.roll(image, int(centerCoord[1]), axis=0)
             # Translate the image based on the change of position
             # The position is in meters, so we need to convert it to pixels
-            changeInPosition = (pastImages[-1][2][0] - position[0], pastImages[-1][2][1] - position[1])
+            changeInPosition = (data["api"]["truckPlacement"]["coordinateX"] - position[0], data["api"]["truckPlacement"]["coordinateZ"] - position[1])
             changeInPosition = (int(changeInPosition[0] * pxPerMeter), int(changeInPosition[1] * pxPerMeter))
             image = np.roll(image, changeInPosition[0], axis=1)
             image = np.roll(image, changeInPosition[1], axis=0)
+            # Fill the rolled over parts with 0
+            if changeInPosition[0] > 0:
+                image[:, :changeInPosition[0]] = 0
+            elif changeInPosition[0] < 0:
+                image[:, changeInPosition[0]:] = 0
+
+            if changeInPosition[1] > 0:
+                image[:changeInPosition[1], :] = 0
+            elif changeInPosition[1] < 0:
+                image[changeInPosition[1]:, :] = 0
             # Draw the images with the alpha
             # frame = image
             frame = cv2.addWeighted(frame, 1, image, alpha, 0)
+            counter += 1
         
+        # Rotate the frame to match the truck rotation
+        frame = rotateImage(frame, -data["api"]["truckPlacement"]["rotationX"]*360, center=(width/2, height/2))
+        cv2.putText(frame, "Images: " + str(counter), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
         data["frame"] = frame
+        
+        # Check if the image is too far away
+        while math.sqrt((pastImages[0][2][0] - data["api"]["truckPlacement"]["coordinateX"])**2 + (pastImages[0][2][1] - data["api"]["truckPlacement"]["coordinateZ"])**2) > maxDistance:
+            pastImages.pop(0)
     except: 
         import traceback
         print(traceback.format_exc())
